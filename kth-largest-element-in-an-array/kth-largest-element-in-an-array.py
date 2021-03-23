3ebabdf6f609a02ba97378735d85459a1b8a7c3e
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-03-22 23:03:51 -0700

    Time: 1192 ms (10.18%), Space: 15.2 MB (44.88%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,17 +1,39 @@
-import heapq
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        """
-            nums = [3,2,1,5,6,4], k = 2
-            
-            heap = [5,6]
-        """
-        pq = [] 
-        for i in range(len(nums)):
-            heapq.heappush(pq, nums[i])
-            if len(pq) > k:
-                heapq.heappop(pq)
+        low, high = 0, len(nums)-1 
         
-        result = heapq.heappop(pq)
-    
-        return result
+        k_smallest = len(nums) - k 
+        while low <= high:
+            res = self.partition(low, high, nums)
+            print(res)
+            if res == k_smallest:
+                return nums[res]
+
+            elif res < k_smallest:
+                low = res + 1 
+
+            else:
+                high = res - 1 
+        
+        return res
+        
+    def partition(self, start, end, nums):
+        pivot = start
+        
+        while start <= end:
+            
+            while start <= end and nums[start] <= nums[pivot]:
+                start += 1 
+            
+            while start <= end and nums[pivot] <= nums[end]:
+                end -= 1 
+            
+            if start > end:
+                break 
+            
+            nums[start], nums[end] = nums[end], nums[start]
+            
+            start += 1 
+            
+        nums[pivot], nums[end] = nums[end], nums[pivot]
+        return end 
