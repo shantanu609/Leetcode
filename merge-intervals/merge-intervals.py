4d8633467e983f022298929250c2f3413e3f85de
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-03-23 23:00:03 -0700

    Time: 76 ms (97.70%), Space: 16.1 MB (85.38%) - LeetHub

--- merge-intervals/merge-intervals.py
+++ merge-intervals/merge-intervals.py
@@ -6,16 +6,18 @@ class Solution:
         """
         intervals.sort(key = lambda x : x[0])
         start, end = intervals[0][0], intervals[0][1]
+        
         mergedIntervals = []
         
         for i in range(1, len(intervals)):
             newStart, newEnd = intervals[i]
+            
             if end < newStart:
                 mergedIntervals.append([start, end])
                 start, end = newStart, newEnd 
             else:
-                if end < newEnd:
-                    end = newEnd 
+                # mergining condition 
+                end = max(end , newEnd)
         
         mergedIntervals.append([start, end])
     
