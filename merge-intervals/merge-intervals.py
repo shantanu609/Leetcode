b1cb60f03c095caec7b7e564c0e5b54efb9eafb8
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-03-23 22:22:27 -0700

    Time: 84 ms (78.78%), Space: 16 MB (97.54%) - LeetHub

---
+++ merge-intervals/merge-intervals.py
@@ -0,0 +1,22 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        """
+            [[2,6], [1,3],[5,10],[15,18]]
+            
+        """
+        intervals.sort(key = lambda x : x[0])
+        start, end = intervals[0][0], intervals[0][1]
+        mergedIntervals = []
+        
+        for i in range(1, len(intervals)):
+            newStart, newEnd = intervals[i]
+            if end < newStart:
+                mergedIntervals.append([start, end])
+                start, end = newStart, newEnd 
+            else:
+                if end < newEnd:
+                    end = newEnd 
+        
+        mergedIntervals.append([start, end])
+    
+        return mergedIntervals
