bea6090782de526620949eaa69c6fc6cdca95095
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-03-25 11:54:08 -0700

    Time: 36 ms (55.49%), Space: 14.7 MB (20.91%) - LeetHub

--- subsets/subsets.py
+++ subsets/subsets.py
@@ -1,24 +1,28 @@
 class Solution:
-    dirs = None 
-    result = None
+    result = None 
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        self.result = []
-        self.backtrack(nums, set(), 0)
+        
+        self.result = [] 
+        self.backtrack(nums, 0, set())
         return self.result 
-
-    def backtrack(self, nums, subset, indx):
-        # base case 
+    
+    def backtrack(self, nums, indx, subset):
         self.result.append(subset.copy())
+        
+        # base case 
         if indx == len(nums):
             return 
         
         # logic 
         for i in range(indx, len(nums)):
-            #action
+            # action 
             subset.add(nums[i])
             
             # recurse
-            self.backtrack(nums, subset, i+1)
+            self.backtrack(nums, i+1, subset)
             
             # backtrack
             subset.remove(nums[i])
+            
+# Time : O(2 ^ n)
+# Space: O(n)
