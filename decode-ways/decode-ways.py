0b503e5a21f196e4f92ec842025dc5fefa967375
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-03-25 11:07:14 -0700

    Time: 28 ms (88.87%), Space: 14.1 MB (95.75%) - LeetHub

--- decode-ways/decode-ways.py
+++ decode-ways/decode-ways.py
@@ -1,27 +1,25 @@
 class Solution:
-    output = None 
-    
     def numDecodings(self, s: str) -> int:
+        n = len(s)
+        dp = [0] * (n+1)
         
-        return self.recurse(s, 0)
+        # initialize the dp array 
+        dp[0] = 1 
+        # check if starting 0th index element in our string is zero or no? it no then 1 else 0 in dp 
+        dp[1] = 1 if s[0] != '0' else 0 
         
-    @lru_cache(maxsize = None)
-    def recurse(self, s , indx):
-        # base case 
-        if indx == len(s):
-            return 1
-
-        if s[indx] == '0':
-            return 0
+        # loop through the remaining string in s from 1 to n 
+        for i in range(1, n):
+            
+            # check if s[i] is not zero? if not then use the current value in dp for dp[i+1]
+            if s[i] != '0':
+                dp[i+1] = dp[i]
+                
+            # check for two digits. it within this range of 10 <= __ <= 26 then add dp[i-1]
+            # dont forget to check for the bound 
+            if i < len(s):
+                temp = int(s[i-1 : i+1])
+                if 10 <= temp <= 26:
+                    dp[i+1] += dp[i-1]
         
-        if indx == len(s) - 1:
-            return 1 
-
-        # logic 
-        # get the substring single digit 
-        answer = self.recurse(s, indx + 1)
-        # get the substring two digit
-        if int(s[indx: indx + 2]) <= 26:
-            answer += self.recurse(s, indx + 2)
-        
-        return answer
+        return dp[-1]
