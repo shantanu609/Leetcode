285329d0016dc224bdadf78b017593176051e2b7
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-03-27 11:16:34 -0700

    Time: 64 ms (75.93%), Space: 15.2 MB (14.56%) - LeetHub

--- maximum-subarray/maximum-subarray.py
+++ maximum-subarray/maximum-subarray.py
@@ -3,7 +3,7 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
         
-        
+        start, end = 0 , 0 
         result = nums[0]
         rSum = nums[0]
         
@@ -11,9 +11,16 @@ class Solution:
         for i in range(1, len(nums)):
             
             # check for the running sum for new nums[i]
+            if nums[i] > rSum + nums[i]:
+                start = i 
             rSum = max(rSum + nums[i], nums[i])
             
             # update the result to store the maximum running sum 
+            if rSum > result:
+                end = i 
             result = max(result, rSum)
+        
+        print(start, end)
+        print(nums[start: end+1])
         
         return result 
