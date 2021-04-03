bfef09c28a9552b6a8192f1039503824c9355bd2
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-04-03 13:12:20 -0700

    Time: 148 ms (99.80%), Space: 20 MB (78.17%) - LeetHub

--- kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -1,36 +1,47 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        count = 0
         n = len(matrix)
         low = matrix[0][0]
-        high = matrix[n-1][n-1]
+        high  = matrix[n-1][n-1]
         
         while low < high:
             
-            mid = (high + low) // 2 
+            mid = (low + high) // 2
             
-            totalCount = self.getCount(matrix, mid)
+            tempLow, tempHigh = matrix[0][0], matrix[n-1][n-1]
             
-            if totalCount < k:
-                low = mid + 1
+            count, tempLow, tempHigh = self.binaryHelper(matrix, mid, tempLow, tempHigh)
+            
+            if count == k:
+                return tempLow 
+        
+            elif count < k:
+                low = tempHigh 
             
             else:
-                high = mid 
+                high = tempLow 
+            
         
         return low 
     
-    def getCount(self, matrix, mid):
-        length = len(matrix)-1 
+    
+    def binaryHelper(self, matrix, mid, tempLow, tempHigh):
+        row = len(matrix) - 1
+        col = 0 
         count = 0 
         
-        for i in range(len(matrix)):
+        # operation is to count the number of elements less than equals to mid 
+        while row >= 0 and col < len(matrix[0]):
             
-            j = length 
+            if matrix[row][col] > mid:
+                # adjust high. Bring it closer to mid 
+                tempHigh = min(tempHigh, matrix[row][col])
+                row -= 1 
             
-            while j >= 0 and matrix[i][j] > mid:
-                j -= 1 
-            
-            count += j+1 
-            
-        return count 
-            
+            else:
+                # shift low, bring it forward towards mid 
+                tempLow = max(tempLow, matrix[row][col])
+                col += 1 
+                count += row + 1 
+        
+        return (count, tempLow, tempHigh)
