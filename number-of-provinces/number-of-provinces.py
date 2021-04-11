c94e66f99d74abe1d58fb62be25fec13f0981cf8
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-04-10 23:49:07 -0700

    Time: 224 ms (22.84%), Space: 14.5 MB (88.14%) - LeetHub

--- number-of-provinces/number-of-provinces.py
+++ number-of-provinces/number-of-provinces.py
@@ -1,22 +1,39 @@
+class DSU:
+    def __init__(self, size):
+        self.parent = [i for i in range(size)]
+    
+    def find(self, num):
+        if self.parent[num] != num:
+            self.parent[num] = self.find(self.parent[num])
+        
+        return self.parent[num]
+
+    def union(self, num1, num2):
+        self.parent[self.find(num1)] = self.find(num2)
+        
+    
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        visited = [0] * len(isConnected)
-        count = 0 
         
-        for row in range(len(isConnected)):
-            if visited[row] == 0:
-                self.dfs(isConnected, row, visited)
-                count += 1 
+        size = len(isConnected)
+        dsu = DSU(size)
+        d = {}
+        count = 0
         
-        return count 
-    
-    def dfs(self, isConnected, row, visited):
-        # base case 
-        
-        
-        # logic 
-        for col in range(len(isConnected[0])):
-            if isConnected[row][col] == 1 and visited[col] == 0:
-                visited[col] = 1 
-                self.dfs(isConnected, col, visited)
+        for i in range(size):
+            one = i 
+            for j in range(len(isConnected[0])):
                 
+                second = j
+                    
+                if isConnected[one][second] == 1 and one != second:
+                    dsu.union(one, second)
+
+        
+        cout = 0 
+        for i in range(len(dsu.parent)):
+            if dsu.parent[i] == i:
+                count += 1 
+            
+        return count
+            
