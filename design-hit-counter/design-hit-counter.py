37bf6fd90d39fda2a76bf505b7f8cd8537f9d395
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-05-17 18:01:46 -0700

    Time: 28 ms (82.27%), Space: 14.4 MB (17.44%) - LeetHub

--- design-hit-counter/design-hit-counter.py
+++ design-hit-counter/design-hit-counter.py
@@ -1,83 +1,55 @@
-from collections import OrderedDict
 class HitCounter:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.d = OrderedDict()
+        self.dq = deque() 
 
     def hit(self, timestamp: int) -> None:
         """
         Record a hit.
         @param timestamp - The current timestamp (in seconds granularity).
         """
-        if timestamp not in self.d:
-            self.d[timestamp] = 0 
+        if not len(self.dq):
+            self.dq.append([timestamp, 1])
+        else:
+            if self.dq[-1][0] != timestamp:
+                self.dq.append([timestamp, 1])
+            else:
+                self.dq[-1][1] += 1 
         
-        self.d[timestamp] += 1 
-        
+        for i in range(len(self.dq)):
+            if i < len(self.dq):
+                a, b = self.dq[i]
+                last = self.dq[-1][0]
+                if last - a >= 300:
+                    self.dq.popleft()
+                else:
+                    break 
+
     def getHits(self, timestamp: int) -> int:
         """
         Return the number of hits in the past 5 minutes.
         @param timestamp - The current timestamp (in seconds granularity).
         """
-        if timestamp <= 300:
-            upperB = timestamp 
-            return self.countHits(timestamp)
-    
-        else: 
-            lowerB = timestamp - 300 
-            list_ = []
-            for k,v in self.d.items():
-                if k > lowerB:
-                    break 
-                list_.append(k)
-            for num in list_:
-                self.d.pop(num)
-            
-            return self.countHits(timestamp)
+        lowerB, upperB = max(0, timestamp - 300), timestamp
         
-    def countHits(self, upperB):
         count = 0 
-        for k,v in self.d.items():
-            if k > upperB:
-                break 
-            count += v 
+        for i in range(len(self.dq)):
+            a, b = self.dq[i]
+            if lowerB < a <= upperB:
+                count += b 
+        
+        return count
+        
+"""
+301 - 300 = 1
+    301 -> lowerB = 2
+           upperB = 301
 
-        return count 
-
-
+"""
 # Your HitCounter object will be instantiated and called as such:
 # obj = HitCounter()
 # obj.hit(timestamp)
 # param_2 = obj.getHits(timestamp)
-
-"""
-    hits = [1,1,1,1,1,1,2,2,2,297, 3,4,5]
-    
-    target = 301
-    lowerB = 1
-    upperB = 301
-    
-    d = {
-            1: 100,0000
-            2: 250,000
-            3: 14,000 
-            4: 500 
-            300: 
-            301: 
-    }
-    
-    target = 3 
-    lower = max(1, 5-300) = 1 
-    upper = 2
-    
-    result = j - i 
-    
-    l            r
-    [1,1,1,1,1,1,2,5]    = r - l + 1 
-    
-    l                           m                         r
-    [z,z,z,z,z,1,1,1,1, 1, 1,1,1,1,1,1,1,...(50), 2,2,2,2,2....2,2,(25), 3, 4, 5, 6, 7] = O(200)
-"""
