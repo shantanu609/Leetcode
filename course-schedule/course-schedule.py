0ca90ca0cad6828b047a389e748ded5cb521c6f5
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-05-18 18:35:56 -0700

    Time: 96 ms (73.54%), Space: 15.3 MB (98.10%) - LeetHub

--- course-schedule/course-schedule.py
+++ course-schedule/course-schedule.py
@@ -1,48 +1,39 @@
+from collections import deque
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        """
-            [ai, bi] indicates that you must take course bi first
-             [1,0]
-                    0 -> 1
-             [[1,0],[0,1]]
-                 0 -> 1 
-                 1 -> 0
-            
-            [[0,1],[0,2],[1,3],[2,4],[3,5],[3,0],[4,5]]
-               
-               1: [0]               
-               2: [0]            
-               3: [1, 0]            
-               4: [2, 0]         
-               5: [3, 1, 0, 2]      
-               0: [3, 1, 0]     <- i
-                   j   
-            
-                [1,0]
-        """
+        outdegree = [0 for _ in range(numCourses)]
+        d = {} 
+        self._formGraph(outdegree, d, prerequisites)
         
-        d = {}
-        for pair in prerequisites:
-            a, b = pair 
-            if b not in d:
-                d[b] = set()
+        q = deque()
+        for i in range(len(outdegree)):
+            if outdegree[i] == 0:
+                q.append(i)
+
+        return self._check(q, outdegree, d)
+
+    def _formGraph(self, outdegree, d, prereq):
+        for pair in prereq:
+            c1, c2 = pair 
+            outdegree[c1] += 1 
             
-            d[b].add(a)
-            
-            if a in d: 
-                for num in d[a]:
-                    d[b].add(num)
-        
-        for parent, childs in d.items():
-            for j in childs:
+            if c2 not in d:
+                d[c2] = [] 
                 
-                if j in d: 
-                    set_ = d[j]
-                    if parent in set_:
-                        return False 
-                else:
-                    continue 
+            d[c2].append(c1)
+    
+    def _check(self, q, outdegree, d):
+        while q: 
+            curr = q.popleft()
+            if curr in d:
+                for neigh in d[curr]:
+
+                    outdegree[neigh] -= 1 
+                    if outdegree[neigh] == 0:
+                        q.append(neigh)
+
+        for degree in outdegree:
+            if degree > 0:
+                return False 
         
-        return True 
-            
-            
+        return True
