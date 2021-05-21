806203abb49d7886c597dcda65d18ec3235c9ec3
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-05-21 13:27:14 -0700

    Time: 68 ms (45.11%), Space: 15.1 MB (42.07%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,39 +1,12 @@
+import heapq
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        low, high = 0, len(nums)-1 
+        pq = []
+        for num in nums:
+            heapq.heappush(pq, num)
+            
+            if len(pq) > k:
+                heapq.heappop(pq)
         
-        k_smallest = len(nums) - k 
-        while low <= high:
-            res = self.partition(low, high, nums)
-            print(res)
-            if res == k_smallest:
-                return nums[res]
-
-            elif res < k_smallest:
-                low = res + 1 
-
-            else:
-                high = res - 1 
-        
-        return res
-        
-    def partition(self, start, end, nums):
-        pivot = start
-        
-        while start <= end:
-            
-            while start <= end and nums[start] <= nums[pivot]:
-                start += 1 
-            
-            while start <= end and nums[pivot] <= nums[end]:
-                end -= 1 
-            
-            if start > end:
-                break 
-            
-            nums[start], nums[end] = nums[end], nums[start]
-            
-            start += 1 
-            
-        nums[pivot], nums[end] = nums[end], nums[pivot]
-        return end 
+        result = heapq.heappop(pq)
+        return result
