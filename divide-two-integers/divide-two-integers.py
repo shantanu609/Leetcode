58dcb8b475b43e3c144adbf51e27af8a6b8bd71c
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-05-21 18:11:11 -0700

    Time: 32 ms (71.08%), Space: 14.2 MB (78.59%) - LeetHub

--- divide-two-integers/divide-two-integers.py
+++ divide-two-integers/divide-two-integers.py
@@ -1,28 +1,46 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        # edge case 
-        if dividend <= (-2**31) and divisor == -1:
-            return (2**31)-1
+        if divisor == 0:
+            return (2**31) - 1
         
-        ldividend, ldivisor = abs(dividend), abs(divisor)
-        result = 0 
+        if dividend == 0:
+            return 0
+    
+        if (dividend < - 2**31 or dividend > 2**31 -1) and (divisor == 1):
+            return (2**31) - 1 
+    
+        if dividend == 1 and (divisor < -2**31 or divisor > 2**31 - 1):
+            return 0 
+    
+        quotient = 0
+        newDivisor = abs(divisor)
+        newDividend = abs(dividend)
         
-        while ldividend >= ldivisor:
+        while newDividend >= newDivisor:
             shift = 0 
             
-            while (ldivisor << shift)  <= ldividend:
+            while (newDivisor << shift) <= newDividend:
                 shift += 1 
             
             shift -= 1 
-            
-            result = result + (1 << shift)
-            
-            ldividend = ldividend - (ldivisor << shift)
         
-        if dividend < 0 and divisor < 0:
-            return result 
+            quotient += (1 << shift)
+            
+            newDividend -= newDivisor << shift 
         
+        if dividend < 0 and divisor < 0: 
+            return self._getResult(quotient)
+    
         if dividend > 0 and divisor > 0:
-            return result 
+            return self._getResult(quotient)
         
-        return -result 
+        return -quotient 
+    
+    def _getResult(self, quotient):
+        if quotient > (2**31 -1 ):
+            quotient =  (2**31 - 1)
+        
+        elif quotient < (-2**31):
+            quotient = (2**31 - 1)
+        
+        return quotient 
