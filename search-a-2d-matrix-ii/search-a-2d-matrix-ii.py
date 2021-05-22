4b6516bcb495a88f56f7a0ea7770b951610c3af8
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-05-21 23:07:59 -0700

    Time: 164 ms (62.02%), Space: 20.5 MB (64.52%) - LeetHub

--- search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
+++ search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -1,16 +1,51 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        row = len(matrix) -1 
-        col = 0 
-        
-        while row >= 0 and col < len(matrix[0]):
-            if matrix[row][col] == target:
+        for i in range(min(len(matrix), len(matrix[0]))):
+            # search in the row
+            result1 = self._binarySearch(matrix, target, i, True)
+            
+            # search in the col 
+            result2 = self._binarySearch(matrix, target, i, False)
+            
+            #check if any of the results could have found the target?
+            if result1 or result2:
                 return True 
-            
-            elif matrix[row][col] < target:
-                col += 1 
-            
-            else:
-                row -= 1 
-            
+        
         return False 
+    
+    def _binarySearch(self, matrix, target, i, vertical):
+        
+        left = 0 
+        right = len(matrix) - 1 if vertical else len(matrix[0]) - 1 
+    
+        while left <= right:
+            
+            mid = (left + right) // 2 
+            
+            # check if we want to search in a row wise fashion or column wise?
+            if vertical: 
+                # column wise 
+                
+                if matrix[mid][i] == target:
+                    return True 
+                
+                elif matrix[mid][i] < target:
+                    left = mid + 1 
+                
+                else:
+                    right = mid - 1 
+                
+            else:
+                # row wise 
+                
+                if matrix[i][mid] == target:
+                    return True 
+            
+                elif matrix[i][mid] < target:
+                    left = mid + 1 
+                
+                else:
+                    right = mid - 1 
+                
+        return False 
+                
