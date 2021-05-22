0dded987bb4a17a6ee251b9afd4ba50daddf09e4
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-05-21 21:32:54 -0700

    Time: 160 ms (80.51%), Space: 20.4 MB (87.81%) - LeetHub

--- search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
+++ search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -1,23 +1,16 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        """
-            1) BF - > 2-D array
-            2) BF apto. -> 1-D -> sort -> search/ bs
-            3) 
-        """
-        row = len(matrix)-1
+        row = len(matrix) -1 
         col = 0 
         
         while row >= 0 and col < len(matrix[0]):
-            num = matrix[row][col]
-            
-            if num == target:
+            if matrix[row][col] == target:
                 return True 
             
-            elif num < target: 
+            elif matrix[row][col] < target:
                 col += 1 
             
             else:
                 row -= 1 
-        
+            
         return False 
