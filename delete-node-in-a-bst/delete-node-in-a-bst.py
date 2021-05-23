c3ab921a8e48c2accaa5c8324b838dc1fcfd37c4
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-05-23 12:30:14 -0700

    Time: 72 ms (78.54%), Space: 18.5 MB (59.57%) - LeetHub

--- delete-node-in-a-bst/delete-node-in-a-bst.py
+++ delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -8,42 +8,36 @@ class Solution:
     def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
         if not root:
             return None 
-
+        
         return self.deleteHelper(root, key)
-
     
     def deleteHelper(self, root, target):
         # base case 
         if not root:
-            return None
+            return None 
         
         # logic 
-        if root.val > target:
-            # search in the left sub tree. 
-            root.left = self.deleteHelper(root.left, target)
-        
-        elif root.val < target:
-            # search in the right sub tree. 
+        if target > root.val:
+            # search in right sub tree 
             root.right = self.deleteHelper(root.right, target)
         
+        elif target < root.val:
+            root.left = self.deleteHelper(root.left, target)
+        
         else:
-            # key found! 
+            # key found. 
             
-            # 1) if no left sub tree to the target node, just return right sub tree. 
             if not root.left:
                 return root.right 
-            
-            # 2) a)  else find the largest node value from the right side of the left subtree and replace. 
-            curr = root 
-            curr = root.left 
-            while curr.right:
-                curr = curr.right 
-            
-            root.val = curr.val 
-            
-            # 2) b) delete this new node, that we just replaced, present in curr 
-            root.left = self.deleteHelper(root.left, curr.val)
         
+            # search for largest number from the left sub tree 
+            node = root.left 
+            
+            while node and node.right:
+                node = node.right 
+            
+            root.val = node.val 
+            
+            root.left = self.deleteHelper(root.left, node.val)
         
         return root
-            
