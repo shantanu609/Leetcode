6f763fb6b80a9ff660288fa579b3e5036ec0b66f
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-05-25 10:25:21 -0700

    Time: 32 ms (78.32%), Space: 14.4 MB (66.69%) - LeetHub

--- symmetric-tree/symmetric-tree.py
+++ symmetric-tree/symmetric-tree.py
@@ -4,20 +4,44 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque 
+from collections import deque
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
         if not root:
             return True 
-        return self.helper(root.left, root.right)
-    
-    def helper(self, node1, node2):
-        # base case 
-        if not node1 and not node2:
-            return True 
         
-        if not node1 or not node2:
-            return False 
+        q = deque([root])
         
-        # logic 
-        return (node1.val == node2.val) and (self.helper(node1.left, node2.right)) and (self.helper(node1.right, node2.left))        
+        while q:
+            size = len(q)
+            p1, p2 = 0, size - 1 
+            while p1 <= p2 :
+                if not q[p1] and not q[p2]:
+                    p1 += 1 
+                    p2 -= 1
+                    continue 
+                    
+                if (not q[p1] and q[p2]) or (q[p1] and not q[p2]) or (q[p1].val != q[p2].val):
+                    return False 
+            
+                p1 += 1 
+                p2 -= 1 
+            
+            for _ in range(size):
+                curr = q.popleft()
+                
+                if curr and curr.left:
+                    q.append(curr.left)
+                elif curr and not curr.left:
+                    q.append(None)
+                
+                if curr and curr.right:
+                    q.append(curr.right)
+                elif curr and not curr.right: 
+                    q.append(None)
+        
+        return True 
+                
+                
+                
+                
