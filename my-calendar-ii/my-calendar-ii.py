516f4dda6b9c84daf075a0b345c59b683665b4de
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-05-30 12:37:36 -0700

    Time: 504 ms (46.09%), Space: 14.8 MB (90.47%) - LeetHub

---
+++ my-calendar-ii/my-calendar-ii.py
@@ -0,0 +1,24 @@
+class MyCalendarTwo:
+
+    def __init__(self):
+        self.overlap = [] 
+        self.calendar = [] 
+
+    def book(self, start: int, end: int) -> bool:
+        
+        for i,j in self.overlap:
+            if start < j and end > i: 
+                # third overlap 
+                return False 
+        
+        for i, j in self.calendar:
+            # if overlap 
+            if start < j and end > i : 
+                self.overlap.append((max(start, i), min(end, j)))
+        
+        self.calendar.append((start, end))
+        return True 
+
+# Your MyCalendarTwo object will be instantiated and called as such:
+# obj = MyCalendarTwo()
+# param_1 = obj.book(start,end)
