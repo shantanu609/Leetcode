fc082895bbf188b18ccd4a719a36be0be4393b65
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-06-05 23:08:28 -0700

    Time: 28 ms (88.38%), Space: 14.6 MB (42.91%) - LeetHub

--- binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
+++ binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
@@ -8,44 +8,32 @@ from collections import deque
 class Solution:
     def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
         if not root:
-            return [] 
-        result = [] 
-        
-        q = deque()
-        flag = False # for left to right 
-        q.append(root)
+            return []
+        output = [] 
+        q = deque([root])
+        reverse = False 
         
         while q:
+            temp = deque()
             size = len(q)
-            temp = []
-            if flag == False:
-                # left to right 
-                for _ in range(size):
-                    curr = q.popleft()
-                    temp.append(curr.val)
-                    if curr.left:
-                        q.append(curr.left)
-                    if curr.right:
-                        q.append(curr.right)
-                flag = True 
-                result.append(temp)
             
-            else:
-                # right to left
-                temp = [] 
-                for _ in range(size):
-                    curr = q.popleft()
-                    temp.append(curr.val)
-                    if curr.left:
-                        q.append(curr.left)
-                    if curr.right:
-                        q.append(curr.right)
+            for _ in range(size):
+                curr = q.popleft() 
+                temp.append(curr.val)
+                if curr.left:
+                    q.append(curr.left)
                 
-                temp = temp[::-1]
-                result.append(temp)
-                flag = False 
-        
-        return result
+                if curr.right: 
+                    q.append(curr.right)
             
-
+            if reverse: 
+                output.append([])
+                while temp:
+                    output[-1].append(temp.pop())
+                reverse = False 
+            else: 
+                output.append(list(temp).copy())
+                reverse = True
         
+        
+        return output
