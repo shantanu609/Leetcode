eceeb3dd7b2344fe9420ae21fba8d1d0d861a46d
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-06-08 11:26:15 -0700

    Time: 6304 ms (11.33%), Space: 14 MB (97.29%) - LeetHub

--- word-search/word-search.py
+++ word-search/word-search.py
@@ -1,36 +1,30 @@
 class Solution:
-    dirs = None 
     def exist(self, board: List[List[str]], word: str) -> bool:
-        self.dirs = [[1,0],[-1,0],[0,1],[0,-1]]
-        visited = [[False for _ in range(len(board[0]))] for _ in range(len(board))]
-        
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] == word[0]:
-                    visited[i][j] = True 
-                    if self.dfs(board, i, j, visited, word, 1):
+                    if self.dfs(board, i, j, word, 0):
                         return True 
-                    visited[i][j] = False 
-        
+    
         return False 
     
-    def dfs(self, board, i, j, visited, word, indx):
+    def dfs(self, board, i, j, word, indx):
         # base case 
-        if indx == len(word):
+        if indx == len(word) -1:
             return True 
         
-        # logic 
-        # mark the i,j as visited 
+        # Logic 
+        dirs = [[1,0],[-1,0],[0,1],[0,-1]]
+        ch = board[i][j]
+        board[i][j] = -2 # visited 
         
-        # explore neighoring skills 
-        for pair in self.dirs:
-            visited[i][j] = True
-            r = pair[0] + i 
-            c = pair[1] + j 
+        for pair in dirs: 
+            r = i + pair[0]
+            c = j + pair[1]
             
-            if 0 <= r < len(board) and 0 <= c < len(board[0]) and visited[r][c] == False and board[r][c] == word[indx]: 
-                if self.dfs(board, r, c, visited, word, indx + 1):
+            if 0 <= r < len(board) and 0 <= c < len(board[0]) and board[r][c] == word[indx+1] :
+                if self.dfs(board, r, c, word, indx + 1):
                     return True 
-            visited[i][j] = False
-        return False 
-        
+            
+        board[i][j] = ch 
+        return False
