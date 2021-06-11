a5d9fba5f4efd6af23db51cc805c29cac4810c2b
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-06-11 07:21:02 -0700

    Time: 152 ms (39.69%), Space: 15.4 MB (50.92%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,55 +1,26 @@
-class DSU:
-    def __init__(self, m):
-        self.parent = [i for i in range(m)]
-        self.count = m
-        
-    def find(self, num):
-        if self.parent[num] != num:
-            self.parent[num] = self.find(self.parent[num])
-        
-        return self.parent[num]
-        
-    
-    def union(self, num1, num2):
-        p1 = self.find(num1)
-        p2 = self.find(num2)
-        if p1 == p2:
-            return False 
-        
-        self.parent[p1] = p2
-        return True 
-
-    def getCount(self):
-        
-        for i in range(len(self.parent)):
-            if self.parent[i] == i:
-                self.count -= 1 
-        
-        return self.count
-    
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-            
-        dsu = DSU(m * n)
-        dirs = [[1,0],[-1,0],[0,1],[0,-1]]
-        count = 0 
-        
+        num = 0 
         for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    count += 1 
-                
-                    for pair in dirs:
-                        r = i + pair[0]
-                        c = j + pair[1]
-                        
-                        if 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c] == '1':
-                            if dsu.union((i * n) + j, (r * n) + c):
-                                count -= 1 
-                    
-        return count
+            for j in range(len(grid[0])): 
+                if grid[i][j] == "1":
+                    self.dfs(grid, i , j)
+                    num += 1 
+        
+        return num 
+    
+    def dfs(self, grid, i, j):
+        # base case 
         
         
+        # Logic 
+        dirs = [[1,0],[-1,0],[0,1],[0,-1]]
+        grid[i][j] = '-2'
+        
+        for pair in dirs: 
+            r = i + pair[0]
+            c = j + pair[1]
+            
+            if 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c] == "1":
+                self.dfs(grid, r, c)
         
