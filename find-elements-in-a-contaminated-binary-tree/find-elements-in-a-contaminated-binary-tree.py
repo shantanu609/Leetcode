a2abf02df807fd39f7df2953d421ef93565bd6e2
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-06-13 11:03:51 -0700

    Time: 100 ms (35.90%), Space: 17.8 MB (96.50%) - LeetHub

--- find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
+++ find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
@@ -5,12 +5,12 @@
 #         self.left = left
 #         self.right = right
 class FindElements:
-
     def __init__(self, root: TreeNode):
         self.root = root 
         self.root.val = 0 
-        self.d = {0:0} 
+        self.d = {0:0}
         self._recover(self.root)
+        # print(self.d)
         
     def _recover(self, root):
         # base case 
@@ -19,22 +19,41 @@ class FindElements:
         
         if root.left:
             root.left.val = root.val * 2 + 1 
-            self.d[root.left.val] = 1 
+            # self.d[root.left.val] = 1 
             self._recover(root.left)
 
         if root.right: 
             root.right.val = root.val * 2 + 2
-            self.d[root.right.val] = 1 
+            # self.d[root.right.val] = 1 
             self._recover(root.right)
         
         
     def find(self, target: int) -> bool:
-        if target not in self.d:
-            return False 
+        binTarget = bin(target+1)[3:]  # string
+        node = self.root 
+        index = 0 
+
+        while index < len(binTarget): 
+
+            if node.val == target:
+                return True 
+            
+            if binTarget[index] == '0':
+                if node.left:
+                    node = node.left 
+                else:
+                    return False
+            
+            else:
+                if node.right:
+                    node = node.right 
+                else:
+                    return False
+            
+            index += 1 
+            
+        return True
         
-        return True 
-
-
 # Your FindElements object will be instantiated and called as such:
 # obj = FindElements(root)
 # param_1 = obj.find(target)
