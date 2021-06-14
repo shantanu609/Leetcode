ed4a97c2b74b6755fc48b8244223468379c48ccc
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-06-14 12:13:58 -0700

    Time: 56 ms (90.22%), Space: 18.1 MB (53.83%) - LeetHub

--- min-stack/min-stack.py
+++ min-stack/min-stack.py
@@ -1,3 +1,4 @@
+import heapq
 class MinStack:
 
     def __init__(self):
@@ -5,24 +6,25 @@ class MinStack:
         initialize your data structure here.
         """
         self.stack = []
+        self.pq = []
+        self.min_ = float('inf')
 
     def push(self, val: int) -> None:
-        if not self.stack:
-            self.stack.append((val, val))
-        else:
-            lastval, lastmin = self.stack[-1]
-            self.stack.append((val, min(min(lastval, lastmin), val)))
+        self.stack.append(val)
+        self.min_ = min(self.min_, val)
 
     def pop(self) -> None:
-        self.stack.pop()
+        curr = self.stack.pop()
+        if curr == self.min_: 
+            self.min_ = float('inf')
+            for num in self.stack:
+                self.min_ = min(self.min_, num)
 
     def top(self) -> int:
-        topval, topmin = self.stack[-1]
-        return topval 
+        return self.stack[-1]
 
     def getMin(self) -> int:
-        topval, topmin = self.stack[-1]
-        return topmin
+        return self.min_
 
 
 # Your MinStack object will be instantiated and called as such:
