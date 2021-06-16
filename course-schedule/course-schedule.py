e2d0420046939821f7527320c9daa95bfc2bcf0b
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-06-16 09:43:54 -0700

    Time: 100 ms (55.71%), Space: 15.5 MB (83.09%) - LeetHub

--- course-schedule/course-schedule.py
+++ course-schedule/course-schedule.py
@@ -1,39 +1,47 @@
 from collections import deque
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        outdegree = [0 for _ in range(numCourses)]
-        d = {} 
-        self._formGraph(outdegree, d, prerequisites)
-        
+        # DS 
+        graph = {} 
+        indegree = [0 for _ in range(numCourses)]
         q = deque()
-        for i in range(len(outdegree)):
-            if outdegree[i] == 0:
-                q.append(i)
-
-        return self._check(q, outdegree, d)
-
-    def _formGraph(self, outdegree, d, prereq):
-        for pair in prereq:
-            c1, c2 = pair 
-            outdegree[c1] += 1 
-            
-            if c2 not in d:
-                d[c2] = [] 
+        
+        for nextCourse, prevCourse in prerequisites:
+            if prevCourse not in graph: 
+                graph[prevCourse] = set()
                 
-            d[c2].append(c1)
-    
-    def _check(self, q, outdegree, d):
+            graph[prevCourse].add(nextCourse)
+            
+            indegree[nextCourse] += 1 
+        
+        for node in range(len(indegree)):
+            if indegree[node] == 0:
+                q.append(node)
+            
+        
         while q: 
-            curr = q.popleft()
-            if curr in d:
-                for neigh in d[curr]:
-
-                    outdegree[neigh] -= 1 
-                    if outdegree[neigh] == 0:
-                        q.append(neigh)
-
-        for degree in outdegree:
-            if degree > 0:
+            size = len(q)
+            for _ in range(size):
+                
+                node = q.popleft() 
+                
+                if node in graph: 
+                    for childNodes in graph[node]:
+                        
+                        indegree[childNodes] -= 1 
+                        
+                        if indegree[childNodes] == 0:
+                            q.append(childNodes)
+        
+        for node in indegree:
+            if node > 0:
                 return False 
         
         return True
+
+"""
+4 - 1 - 3
+  \ 2 /
+"""
+            
+            
