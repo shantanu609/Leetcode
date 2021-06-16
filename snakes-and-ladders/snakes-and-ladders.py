c59a39b46174ed91e1f3c5b334cd0c3074415568
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-06-16 16:51:27 -0700

    Time: 112 ms (75.29%), Space: 14.6 MB (9.60%) - LeetHub

--- snakes-and-ladders/snakes-and-ladders.py
+++ snakes-and-ladders/snakes-and-ladders.py
@@ -1,39 +1,35 @@
 from collections import deque
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        d = {1: 0} 
-        q = deque()
-        q.append(1)
+        visited = set() 
         n = len(board)
+        q = deque([(1,0)])
         
-        while q:
-            curr = q.popleft()
-            if curr == n*n:
-                return d[curr]
-
-            step = curr + 1
+        while q: 
+            tag, moves = q.popleft()
+            r, c = self.getRowCol(tag, n)
+            if board[r][c] != -1: 
+                tag = board[r][c]
             
-            for step in range(curr + 1, min(curr+6, n*n) +1):
-                r, c = self.get(step, n)
+            if tag == n*n:
+                return moves 
+            
+            for nextSteps in range(1, 6+1): 
+                newTag = tag + nextSteps
                 
-                if board[r][c] != -1:
-                    step = board[r][c]
-                
-                if step not in d:
-                    d[step] = d[curr] + 1 
-                    q.append(step)
-                
+                if newTag <= n*n and newTag not in visited: 
+                    visited.add(newTag)
+                    q.append((newTag, moves + 1))
         
-        return -1 
-
-    def get(self, num, n):
-        div = (num-1) // n 
-        mod = (num-1) % n 
+        return -1
+    
+    
+    def getRowCol(self, tag, n):
+        r, c = divmod(tag - 1, n)
         
-        row = n - 1 - div 
+        if r % 2 == 0: 
+            return (n-1-r, c) 
         
-        col = mod if row % 2 != (n % 2) else n - 1 - mod
-        
-        return (row, col)
-                
+        else: 
+            return (n-1-r, n-1-c)
         
