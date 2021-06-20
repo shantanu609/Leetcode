0c4aa32470d0ab2c7e45f90270f01ffce89d16ba
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-06-20 11:10:19 -0700

    Time: 32 ms (97.88%), Space: 14.7 MB (20.25%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,26 +1,50 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left = 0 
-        right = len(nums) -1 
+        left, right = 0, len(nums) - 1 
         
-        while left <= right:
+        while left <= right: 
+            
             mid = (left + right) // 2 
             
             if nums[mid] == target:
                 return mid 
             
-            elif nums[left] <= nums[mid]: 
-                if nums[left] <= target and target < nums[mid]:
-                    right = mid - 1
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target  and target < nums[mid]: 
+                    right = mid - 1 
                 
                 else:
-                    left = mid + 1  
+                    left = mid + 1 
+            
+            else:
+                if nums[mid] < target and target <= nums[right]: 
+                    left = mid + 1 
                 
-            else: 
-                if nums[right] >= target and target > nums[mid]:
-                    left = mid + 1
                 else:
-                    right = mid -1 
+                    right = mid - 1 
         
         return -1
-                
+     
+"""          lr
+    [4,5,6,7,0,1,2]
+             m      
+             
+     l  r        
+    [3, 1]
+     m
+    
+    if nums[low] < nums[mid] : 
+        if target >= nums[low] and target < nums[mid]: 
+                high = mid - 1 
+        else:
+                low = mid + 1 
+    
+    else:
+        if target > nums[mid] and target <= nums[high]: 
+            low = mid + 1 
+        
+        else: 
+            high = mid - 1 
+            
+    
+"""
