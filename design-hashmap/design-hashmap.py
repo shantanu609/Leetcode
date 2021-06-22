813a4e99a2a97482855816765eceaa1ba504a655
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-06-21 19:56:59 -0700

    Time: 260 ms (61.70%), Space: 17.9 MB (30.39%) - LeetHub

--- design-hashmap/design-hashmap.py
+++ design-hashmap/design-hashmap.py
@@ -1,49 +1,77 @@
+class ListNode:
+    def __init__(self, k, v):
+        self.k = k 
+        self. v = v 
+        self.next = None 
+        
 class MyHashMap:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.size = 2000
-        self.map = [None] * self.size
+        self.size = 2069
+        self.hashmap = [None for _ in range(self.size)]
 
     def put(self, key: int, value: int) -> None:
         """
         value will always be non-negative.
         """
-        index = key % self.size 
-        if self.map[index] == None:
-            self.map[index] = [None] * self.size 
+        node = None 
+        hash_key = self._getHash(key)
+        if self.hashmap[hash_key] == None: 
+            self.hashmap[hash_key] = ListNode(-1,-1)
+            node = self.hashmap[hash_key]
         
-        self.map[index][key // self.size] = value 
-   
+        else: 
+            tempNode = self.hashmap[hash_key]
+            prev = None 
+            
+            while tempNode: 
+                prev = tempNode 
+                if tempNode.k == key:
+                    tempNode.v = value
+                    return 
+                tempNode = tempNode.next 
+            
+            node = prev 
+        
+        node.next = ListNode(key, value)
+        
+
     def get(self, key: int) -> int:
         """
         Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
         """
-        index = key % self.size 
+        hash_key = self._getHash(key)
+        tempNode = self.hashmap[hash_key]
+        while tempNode:
+            if tempNode.k == key: 
+                return tempNode.v 
+            tempNode = tempNode.next 
         
-        if self.map[index] == None:
-            return -1 
-    
-        res = self.map[index][key//self.size] 
-        
-        if res is None:
-            return -1 
-        
-        return res
- 
+        return -1 
+
     def remove(self, key: int) -> None:
         """
         Removes the mapping of the specified value key if this map contains a mapping for the key
         """
+        hash_key = self._getHash(key)
+        tempNode = self.hashmap[hash_key]
         
-        index = key % self.size 
-        if self.map[index] == None: 
+        if not tempNode: 
             return 
         
-        self.map[index][key // self.size] = None
+        prev = None 
+        while tempNode: 
+            if tempNode.k == key: 
+                prev.next = tempNode.next 
+                return 
+            prev = tempNode 
+            tempNode = tempNode.next 
         
+    def _getHash(self, key):
+        return key % self.size
 
 
 # Your MyHashMap object will be instantiated and called as such:
@@ -51,3 +79,4 @@ class MyHashMap:
 # obj.put(key,value)
 # param_2 = obj.get(key)
 # obj.remove(key)
+# 1,000,000
