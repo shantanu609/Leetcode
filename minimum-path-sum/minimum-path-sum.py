378b4a1df88c16a592e2dc485273b64f47de795b
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-06-22 23:42:30 -0700

    Time: 116 ms (28.79%), Space: 15.7 MB (60.92%) - LeetHub

--- minimum-path-sum/minimum-path-sum.py
+++ minimum-path-sum/minimum-path-sum.py
@@ -3,18 +3,22 @@ class Solution:
         dp = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
         
         for i in range(len(grid)-1, -1, -1):
-            for j in range(len(grid[0]) -1, -1, -1):
+            for j in range(len(grid[0])-1, -1, -1):
                 
-                if i == len(grid)-1 and j != len(grid[0]) -1 : 
-                    dp[i][j] = grid[i][j] + dp[i][j+1]
+                if i == len(grid)-1 and j != len(grid[0])-1: 
+                    dp[i][j] = dp[i][j+1] + grid[i][j]
+                    continue 
                 
-                elif i != len(grid)-1 and j == len(grid[0]) -1 : 
+                if i != len(grid)-1 and j == len(grid[0])-1:
                     dp[i][j] = grid[i][j] + dp[i+1][j]
+                    continue 
                 
-                elif i != len(grid)-1 and j != len(grid[0])-1: 
-                    dp[i][j] = grid[i][j] + min(dp[i+1][j], dp[i][j+1])
+                if i != len(grid)-1 and j != len(grid[0])-1: 
+                    dp[i][j] = grid[i][j] + min(dp[i][j+1], dp[i+1][j])
+                    continue
                 
-                else:
-                    dp[i][j] = grid[i][j]
+                dp[i][j] = grid[i][j]
         
         return dp[0][0]
+                
+                 
