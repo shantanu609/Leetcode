f102e82b2c2004ebbe6778d194153b0cde3a64ab
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-06-23 00:44:01 -0700

    Time: 24 ms (95.98%), Space: 14.3 MB (60.82%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,38 +1,38 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        # edge case 
         if len(digits) == 0:
-            return []
-        
-        d = {
-            '2' : "abc",
-            '3' : "def",
-            '4' : "ghi",
-            '5' : "jkl",
-            '6' : "mno",
-            '7' : "pqrs",
-            '8' : "tuv",
-            '9' : "wxyz"
-        } 
-        
+            return ''
         self.res = [] 
-        self.backtrack(digits, 0, [], d)
+        self.d = {
+            '2' : "abc",
+            "3" : "def",
+            "4" : "ghi", 
+            "5" : "jkl",
+            "6" : "mno",
+            "7" : "pqrs",
+            "8" : "tuv", 
+            "9" : "wxyz"
+        }
+        
+        self.backtrack(digits, 0, [])
         return self.res 
     
-    def backtrack(self, digit, indx, path, d):
+    def backtrack(self, digits, indx, path):
         # base case 
-        if indx == len(digit): 
+        if indx == len(digits):
             self.res.append(''.join(path))
-            return
+            return 
         
-        # Logic 
-        letters = d[digit[indx]]
+        # logic 
+        letters = self.d[digits[indx]]
         
         for i in range(len(letters)):
             # action 
             path.append(letters[i])
             
             # recurse 
-            self.backtrack(digit, indx + 1, path, d)
+            self.backtrack(digits, indx+1, path)
             
             # backtrack
             path.pop()
