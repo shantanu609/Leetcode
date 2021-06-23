02a4a058d5a5e30ef3d64189722814606c23c451
shantanu609 <54551714+shantanu609@users.noreply.github.com>
2021-06-23 01:17:57 -0700

    Time: 136 ms (64.53%), Space: 15 MB (90.81%) - LeetHub

--- find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,36 +1,29 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        # s = "cbaebabacd", p = "abc"
-        
-        if len(p) > len(s):
-            return []
-        
-        mapP = {}
+        pmap = {} 
         for i in range(len(p)):
-            if p[i] not in mapP:
-                mapP[p[i]] = 0 
+            if p[i] not in pmap:
+                pmap[p[i]] = 0 
             
-            mapP[p[i]] += 1 
+            pmap[p[i]] += 1 
         
-        mapS = {} 
+        smap = {} 
         result = [] 
         
         for i in range(len(s)):
-            if s[i] not in mapS:
-                mapS[s[i]] = 0 
-                
-            mapS[s[i]] += 1 
+            if s[i] not in smap:
+                smap[s[i]] = 0 
+            
+            smap[s[i]] += 1 
             
             if i >= len(p):
-                if mapS[s[i-len(p)]] == 1: 
-                    mapS.pop(s[i - len(p)])
-                
+                if smap[s[i - len(p)]] == 1:
+                    smap.pop(s[i - len(p)])
+                    
                 else:
-                    mapS[s[i-len(p)]] -= 1 
+                    smap[s[i - len(p)]] -= 1 
             
-            
-            if mapP == mapS:
-                result.append(i - len(p) + 1)
+            if smap == pmap:
+                result.append(i + 1 - len(p))
         
-            
         return result
